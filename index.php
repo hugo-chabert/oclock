44d838087e66d2d9a6a387c76d694b139cf93a4c
Hugo <hugo.chabert@laplateforme.io>
2022-05-02 15:05:07 +0200

    Index done

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="public/css/root.css">
+    <script type='text/javascript' src="public/js/script.js"></script>
+    <title>Accueil</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <h1>O'Clock</h1>
+            <div class="buttons">
+                <a href="view/chronometre.php"><button class="button">Chronometre</button></a>
+                <a href="view/horloge.php"><button class="button">Horloge</button></a>
+                <a href="view/minuteur.php"><button class="button">Minuteur</button></a>
+                <a href="view/reveil.php"><button class="button">Réveil</button></a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
