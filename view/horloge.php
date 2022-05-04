432f0d89299729b89f7e6b123ca44ffd85f9cf64
Hugo <hugo.chabert@laplateforme.io>
2022-05-04 11:31:07 +0200

    horloge done

---
+++ view/horloge.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/horloge.css">
+    <script type="text/javascript" src="../public/js/horloge.js"></script>
+    <title>Horloge</title>
+</head>
+<body>
+    <main>
+        <div id="clock">8:10:45</div>
+    </main>
+</body>
+</html>
